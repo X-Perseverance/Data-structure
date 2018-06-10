070ce5b0fc07dcbbbeb47bab1642207c679c68f8
X-Perseverance <1742763760@qq.com>
2018-06-10 17:13:09 +0800

    链表面试题

---
+++ 链表面试题/PrintTailToHead.c
@@ -0,0 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//方法一：非递归
+void SLitsPrintTailToHead(SListNode* pHead)
+{
+	assert(pHead);
+	SListNode* end = NULL;
+	while (end != pHead)
+	{
+		SListNode* cur = pHead;
+		while (cur->_next != end)
+		{
+			cur = cur->_next;
+		}
+		printf("%d ", cur->_data);
+		end = cur;
+	}
+	printf("\n");
+}
+
+//方法二：递归
+void SLitsPrintTailToHeadR(SListNode* pHead)
+{
+	if (pHead == NULL)
+	{
+		return;
+	}
+	SLitsPrintTailToHeadR(pHead->_next);
+	printf("%d ", pHead->_data);
+}
